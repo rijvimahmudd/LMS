40bf8b9c15c9fb2d6a1ef0535a3037dcb7104c95
rijvimahmudd <93908058+rijvimahmudd@users.noreply.github.com>
2023-10-27 22:08:41 +0600

    complete course feature

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,25 +1,49 @@
 import express from 'express';
 import { authorizedRoles, isAuthenticated } from '../utils/auth';
 import {
-	editCourse,
-	getSingleCourse,
-	uploadCourse,
+  addAnswer,
+  addQuestion,
+  addReplyToReview,
+  addReview,
+  editCourse,
+  getAllCourses,
+  getCourseByUser,
+  getSingleCourse,
+  uploadCourse,
 } from '../controllers/course.controller';
 const router = express.Router();
 
 router.post(
-	'/create-course',
-	isAuthenticated,
-	authorizedRoles('admin'),
-	uploadCourse
+  '/create-course',
+  isAuthenticated,
+  authorizedRoles('admin'),
+  uploadCourse,
 );
 
 router.patch(
-	'/edit-course/:id',
-	isAuthenticated,
-	authorizedRoles('admin'),
-	editCourse
+  '/edit-course/:id',
+  isAuthenticated,
+  authorizedRoles('admin'),
+  editCourse,
 );
 
 router.get('/get-course/:id', getSingleCourse);
+
+router.get('/get-courses', getAllCourses);
+
+router.get('/get-course-content/:id', isAuthenticated, getCourseByUser);
+
+router.patch('/add-question', isAuthenticated, addQuestion);
+
+router.patch('/add-answer', isAuthenticated, addAnswer);
+
+router.patch('/add-review/:id', isAuthenticated, addReview);
+
+router.patch(
+  '/add-reply',
+  isAuthenticated,
+  authorizedRoles('admin'),
+  addReplyToReview,
+);
+
 export default router;
