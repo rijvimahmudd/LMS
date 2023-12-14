5f24a73fb528d3ce93882e9c0ac5c2bc75eb6056
rijvimahmudd <93908058+rijvimahmudd@users.noreply.github.com>
2023-12-14 18:06:15 +0600

    added cron jobs to check the notification deletion

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { catchAsyncError } from '../middleware/catchAsyncError';
 import Notification from '../models/notification.model';
 import ErrorHandler from '../utils/ErrorHandler';
+import cron from 'node-cron';
 
 // get all notifications -- only admin
 export const getNotifications = catchAsyncError(
@@ -40,3 +41,15 @@ export const updateNotification = catchAsyncError(
     });
   },
 );
+
+// delete notification
+cron.schedule('0 0 0 * * *', async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  await Notification.deleteMany({
+    status: 'read',
+    createdAt: {
+      $lt: thirtyDaysAgo,
+    },
+  });
+  console.log('deleted 30 days old and read notifications');
+});
